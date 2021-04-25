f97a7d002de6e76bcfb1379eecffff6193647691
marcinopalinski <marcinopalinskito@gmail.com>
2021-04-25 20:41:11 +0200

    Add two child classes

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marcin Opaliński on 25/04/2021.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,15 @@
+//
+// Created by Marcin Opaliński on 25/04/2021.
+//
+
+#ifndef TIC_TAC_TOE_PLAYER_H
+#define TIC_TAC_TOE_PLAYER_H
+
+
+class Player
+{
+
+};
+
+
+#endif //TIC_TAC_TOE_PLAYER_H
