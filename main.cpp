2159e5bb5a3f55a3bf51e8774d048dfbd7b56f26
Marcin Opaliński <marcinopalinskito@gmail.com>
2021-05-29 11:36:00 +0200

    Move class Game to separate file.

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <ctime>
 #include "user.hpp"
 #include "plane.hpp"
+#include "Game.hpp"
 
 int main()
 {
     int game_size, x_player, o_player;
-    srand(time(NULL));
+    srand(time(nullptr));
     std::cout << "Enter game size, X player type and O player type: " << std::endl;
     std::cin >> game_size >> x_player >> o_player;
     Game game(game_size, x_player, o_player);
--- user.hpp
+++ user.hpp
@@ -32,25 +32,4 @@ public:
     [[nodiscard]] Move decide_move(const Plane& plane) const;
 };
 
-class Game
-{
-private:
-    Plane plane;
-    User* players[2]{}; // two players - we can put Player or Bot inside
-    char l_sign{};
-    Game() = delete;
-    Game(const Game &game) = delete;
-    Game(const Game &&game) = delete ;
-    Game& operator = (const Game& game) = delete;
-public:
-    Game(int size, int x_player_mode, int o_player_mode); // player_mode=0-human, other-computer
-    ~Game();
-    [[nodiscard]] bool is_finished() const; // check if game is finished
-    [[nodiscard]] Plane last_plane() const;
-    [[nodiscard]] char last_sign() const;
-    bool forward();
-    // bool save(std::string filename) const; // save current state of game to file
-    // bool load(std::string filename); // load state of game from file
-};
-
 #endif
