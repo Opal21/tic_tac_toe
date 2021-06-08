e2b5b6e9eba6069524f311c50bebfcf20d360ffb
Marcin Opaliński <marcinopalinskito@gmail.com>
2021-06-08 09:35:34 +0200

    Change the input message

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     int x_player, y_player, size = 4;
     std::cout << "Enter the size of arena: ";
     std::cin >> size;
-    std::cout << "Enter player types (X, O): ";
+    std::cout << "Enter player types (0 for user other number for computer) separated by a space: ";
     std::cin >> x_player >> y_player;
     Game game(size, x_player, y_player);
     while (game.forward())
